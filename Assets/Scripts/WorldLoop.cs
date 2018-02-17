e506835e1555981a24fe0fb5b58051a71d750372
Nikita Starshov <starshov.n.a@gmail.com>
2018-02-17 12:31:24 +0300

    First comit, add Entitas, startr generate

---
+++ Assets/Scripts/WorldLoop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Entitas;
+using Entitas.CodeGeneration.Plugins;
+
+namespace GameSystem
+{
+    public class WorldLoop : MonoBehaviour
+    {
+
+        private Systems _systems;
+        private Contexts _contexts;
+        
+        
+        void Start () {
+		
+        }
+	
+        
+        void Update () {
+		
+        }
+    }
+
+}
